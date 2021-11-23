2ee0edba56e01bb716ed775a4159e63717e2171e
谢文通 <xiewentong@cecdat.com>
2021-11-23 14:18:13 +0800

    fix: 生产地址修改

--- src/config/production.js
+++ src/config/production.js
@@ -1,5 +1,5 @@
 window.__smp_config = {
-  REACT_ENV_API_URL: 'http://smp-server.dev-app.cecdata.com',
+  REACT_ENV_API_URL: 'http://smp-server.cecdata.com',
   ICON_URL: 'http://www.cecdata.com/static-file/cec-icon/index',
   UAC_URL: '/static-file/uac-common-js-web/index',
   APP_CODE: 'RB'
